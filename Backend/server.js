1aa2d99d1b611e43f2f47fb6a34ca5d3dc2f8813
punsara yapa <yapapunsara2@gmail.com>
2025-12-01 15:13:35 +0530

    update server.js for deployments
    update server.js for deployments

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ import jwt from "jsonwebtoken";
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://novaai0.netlify.app/", credentials: true }));
 app.use(bodyParser.json());
 
 // ------------------- AUTH0 SESSION CONFIG --------------------
